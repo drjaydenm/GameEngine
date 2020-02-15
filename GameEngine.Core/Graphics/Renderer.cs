6bf3f615a3fc4b3f1c8b088b5eea10b3010e9164
Jayden <drjaydenm@hotmail.com>
2020-02-16 09:47:26 +1000

    Updated the renderer to use material parameters

--- GameEngine.Core/Graphics/Renderer.cs
+++ GameEngine.Core/Graphics/Renderer.cs
@@ -1,6 +1,4 @@
 ﻿using System.Numerics;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using Veldrid;
 
 namespace GameEngine.Core.Graphics
@@ -17,27 +15,16 @@ namespace GameEngine.Core.Graphics
         private readonly Scene scene;
         private readonly CommandList commandList;
 
-        private SceneCameraInfo cameraInfo;
-        private SceneLightingInfo lightingInfo;
-
         public Renderer(Engine engine, Scene scene)
         {
             this.engine = engine;
             this.scene = scene;
             commandList = engine.CommandList;
-
-            CreateBuffers();
         }
 
         public void Draw()
         {
             var camera = scene.ActiveCamera;
-            commandList.UpdateBuffer(ViewProjBuffer, 0, camera.View);
-            commandList.UpdateBuffer(ViewProjBuffer, 64, camera.Projection);
-
-            UpdateStructs();
-            commandList.UpdateBuffer(CameraBuffer, 0, cameraInfo);
-            commandList.UpdateBuffer(LightingBuffer, 0, lightingInfo);
 
             for (var i = 0; i < scene.Entities.Count; i++)
             {
@@ -49,10 +36,22 @@ namespace GameEngine.Core.Graphics
 
                     renderable.UpdateBuffers(commandList);
 
-                    renderable.Material.Bind(commandList, this, renderable.LayoutDescription);
+                    renderable.Material.SetValue("World", renderable.WorldTransform);
+                    renderable.Material.SetValue("View", camera.View);
+                    renderable.Material.SetValue("Projection", camera.Projection);
 
-                    // TODO this should be optimised, ~25% of frame time is spent here
-                    commandList.UpdateBuffer(WorldBuffer, 0, renderable.WorldTransform);
+                    renderable.Material.SetValue("LightDirection", LightDirection);
+                    renderable.Material.SetValue("LightColor", new Vector4(0.5f, 0.5f, 0.5f, 1));
+                    renderable.Material.SetValue("LightIntensity", 2);
+                    renderable.Material.SetValue("AmbientLight", new Vector4(0.4f, 0.4f, 0.4f, 1));
+                    renderable.Material.SetValue("FogColor", RgbaFloat.CornflowerBlue);
+                    renderable.Material.SetValue("FogStartDistance", 60);
+                    renderable.Material.SetValue("FogEndDistance", 150);
+
+                    renderable.Material.SetValue("CameraDirection", camera.ViewDirection);
+                    renderable.Material.SetValue("CameraPosition", camera.Position);
+
+                    renderable.Material.Bind(commandList, this, renderable.LayoutDescription);
 
                     commandList.SetVertexBuffer(0, renderable.VertexBuffer);
                     commandList.SetIndexBuffer(renderable.IndexBuffer, IndexFormat.UInt32);
@@ -65,62 +64,6 @@ namespace GameEngine.Core.Graphics
                         instanceStart: 0);
                 }
             }
-        }
-
-        private void CreateBuffers()
-        {
-            var factory = engine.GraphicsDevice.ResourceFactory;
-
-            ViewProjBuffer = factory.CreateBuffer(new BufferDescription((uint)Unsafe.SizeOf<Matrix4x4>() * 2, BufferUsage.UniformBuffer | BufferUsage.Dynamic));
-            WorldBuffer = factory.CreateBuffer(new BufferDescription((uint)Unsafe.SizeOf<Matrix4x4>(), BufferUsage.UniformBuffer | BufferUsage.Dynamic));
-            CameraBuffer = factory.CreateBuffer(new BufferDescription((uint)Unsafe.SizeOf<SceneCameraInfo>(), BufferUsage.UniformBuffer | BufferUsage.Dynamic));
-            LightingBuffer = factory.CreateBuffer(new BufferDescription((uint)Unsafe.SizeOf<SceneLightingInfo>(), BufferUsage.UniformBuffer | BufferUsage.Dynamic));
-        }
-
-        private void UpdateStructs()
-        {
-            cameraInfo.CameraDirection = scene.ActiveCamera.ViewDirection;
-            cameraInfo.CameraPosition = scene.ActiveCamera.Position;
-
-            // TODO move these all to public variables
-            lightingInfo.LightDirection = LightDirection;
-            lightingInfo.LightColor = new RgbaFloat(0.5f, 0.5f, 0.5f, 1);
-            lightingInfo.LightIntensity = 2;
-            lightingInfo.AmbientLight = new RgbaFloat(0.4f, 0.4f, 0.4f, 1);
-            lightingInfo.FogColor = RgbaFloat.CornflowerBlue;
-            lightingInfo.FogStartDistance = 60;
-            lightingInfo.FogEndDistance = 150;
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        struct SceneCameraInfo
-        {
-            public Vector3 CameraDirection;
-            private float _padding1;
-            public Vector3 CameraPosition;
-            private float _padding2;
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        struct SceneLightingInfo
-        {
-            public Vector3 LightDirection;
-            private float _padding1;
-            public RgbaFloat LightColor;
-            public float LightIntensity;
-            private float _padding2;
-            private float _padding3;
-            private float _padding4;
-            public RgbaFloat AmbientLight;
-            public RgbaFloat FogColor;
-            public float FogStartDistance;
-            private float _padding5;
-            private float _padding6;
-            private float _padding7;
-            public float FogEndDistance;
-            private float _padding8;
-            private float _padding9;
-            private float _padding10;
         }
     }
 }
